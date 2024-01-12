47234e15f356d136b4a6ec6f66d3792a7525579b
unknown <pradyumna_dole@yahoo.com>
2024-01-12 16:01:37 +0530

    my latest code

---
+++ src/basic/B24MyLatestClass.java
@@ -0,0 +1,12 @@
+package basic;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class B24MyLatestClass {
+	@Test
+	public void testcase1() {
+		ChromeDriver driver = new ChromeDriver();
+	}
+
+}
